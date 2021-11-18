90788901c7f720016487a0616cb5cc4789598b37
boshko.koloski <boshko.koloski@gmail.com>
2021-11-18 17:23:07 +0100

    FIX sent_trans #10

--- c19/representations/sent_trans.py
+++ c19/representations/sent_trans.py
@@ -1,15 +1,20 @@
-from sklearn import preprocessing
+""" sentence BERT transformation """
 from sentence_transformers import SentenceTransformer
 
 
 class BERTTransformer():
+    """[summary]
+    """
     def __init__(self, model='distilbert-base-nli-mean-tokens'):
         """Inits the model.
 
         Args:
-            model (str, optional):  Sentence-Transfomer model to be used from here https://www.sbert.net/docs/pretrained_models.html . Defaults to 'distilbert-base-nli-mean-tokens'. Defaults to 'distilbert-base-nli-mean-tokens'.
+            model (str, optional):  Sentence-Transfomer model to be used from here
+            https://www.sbert.net/docs/pretrained_models.html
+            Defaults to 'distilbert-base-nli-mean-tokens'.
         """
-        self._model = model
+        self._model_name = model
+        self._model = None
 
     def fit(self, texts):
         """Fits the Sentence Transformers representation for a given model.
@@ -17,13 +22,30 @@ class BERTTransformer():
         Args:
             texts ([str]): Textual data to be transformed to numerical representation.
         """
-        self._model = SentenceTransformer(self._model)
+        del texts
+        self._model = SentenceTransformer(self._model_name)
+        return self
+
 
     def transform(self, texts):
-        X = self._model.encode(texts)
-        return X
+        """[summary]
 
-    def fit_transform(self,
-                      texts):
+        Args:
+            texts ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        return self._model.encode(texts)
+
+    def fit_transform(self,                    texts):
+        """[summary]
+
+        Args:
+            texts ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
         self.fit(texts)
         return self.transform(texts)
