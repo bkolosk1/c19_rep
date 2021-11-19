d77d229a00921a7a999acce6e979da0aa63e5167
boshko.koloski <boshko.koloski@gmail.com>
2021-11-19 10:00:13 +0100

    FIX statistical linter #10.

--- c19/representations/statistical.py
+++ c19/representations/statistical.py
@@ -1,74 +1,136 @@
-import numpy as np
-from sklearn import preprocessing
-from nltk import word_tokenize
+"""
+    Document representation based on statistical distributions.
+"""
 from string import punctuation
+import numpy as np
+from sklearn.preprocessing import scale
+from nltk import word_tokenize
+
+def get_features():
+    """Returns the feature names
+
+    Returns:
+        list(str): list of strings containing names
+    """
+    return ['min_len', 'max_len', 'upper', 'lower', 'mean', 'digits',
+        'letters', 'spaces', 'punct']
+
+
+def count_vowels(text):
+    """Counts vowels
+
+    Args:
+        text ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    vowels = "aeiou"
+    v_dict = {}
+    for vow in vowels:
+        v_dict[vow] = text.count(vow)
+    return v_dict
+def count_word_based(text):
+    """Counts word based statistics.
+
+    Args:
+        text ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    word_stats = {}
+    stat = []
+    upper = 0
+    lower = 0
+    parsed = word_tokenize(text)
+    stat = [len(word) for word in parsed]
+    min_len = min(stat)
+    max_len = max(stat)
+    upper = sum([1 if word[0].isupper() else 0 for word in parsed])
+    lower = len(stat) - upper
+    stat = np.array(stat)
+    word_stats['min_len'] = min_len
+    word_stats['max_len'] = max_len
+    word_stats['upper'] = upper
+    word_stats['lower'] = lower
+    word_stats['mean'] = np.mean(stat)
+    word_stats['std'] = np.std(stat)
+    return np.array(list(word_stats.values()))
+
+def count_char_based(text):
+    """Counts char based statistics.
+
+    Args:
+        text ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    char_stats = {'digits': 0, 'letters': 0, 'spaces': 0, 'punct': 0}
+    for char in text:
+        if char.isdigit():
+            char_stats['digits'] = char_stats['digits'] + 1
+        elif char.isalpha():
+            char_stats['letters'] = char_stats['letters'] + 1
+        elif char.isspace():
+            char_stats['spaces'] = char_stats['spaces'] + 1
+        elif char in punctuation:
+            char_stats['punct'] = char_stats['punct'] + 1
+    char_stats.update(count_vowels(text))
+    return np.array(list(char_stats.values()))
 
 
 class Stat():
-    def __init__(self):
-        """[summary]
-        """
-        pass
+    """
+        Statistical representation.
+    """
+    def __init__(self,normalize=True):
+        """Inits the model.
 
-    def fit(self, texts, normalize=True):
+        Args:
+            normalize (bool, optional): Normalizaiton of model. Defaults to True.
+        """
+        self.normalize = normalize
+
+    def fit(self, texts):
+        """[summary]
+
+        Args:
+            texts ([type]): [description]
+            normalize (bool, optional): [description]. Defaults to True.
+
+        Returns:
+            [type]: [description]
+        """
         df_data = {}
-        df_data['w_based'] = np.array(list(map(self.count_word_based, texts)))
-        df_data['c_based'] = np.array(list(map(self.count_char_based, texts)))
+        df_data['w_based'] = np.array(list(map(count_word_based, texts)))
+        df_data['c_based'] = np.array(list(map(count_char_based, texts)))
         feature_mat = np.concatenate((df_data['w_based'], df_data['c_based']),
                                      axis=1)
-        X = preprocessing.scale(feature_mat) if normalize else feature_mat
-        return X
+        final_rep = scale(feature_mat) if self.normalize else feature_mat
+        return final_rep
 
-    def fit_transform(self, texts, normalize=True):
-        return self.fit(texts, normalize=True)
+    def fit_transform(self, texts):
+        """[summary]
 
-    def transform(self, texts, normalize=True):
-        return self.fit(texts, normalize=True)
+        Args:
+            texts ([type]): [description]
+            normalize (bool, optional): [description]. Defaults to True.
 
-    def get_features():
-        f = [
-            'min_len', 'max_len', 'upper', 'lower', 'mean', 'digits',
-            'letters', 'spaces', 'punct'
-        ]
-        return f
+        Returns:
+            [type]: [description]
+        """
+        return self.fit(texts)
 
-    def count_vowels(self, text):
-        vowels = "aeiou"
-        v_dict = {}
-        for v in vowels:
-            v_dict[v] = text.count(v)
-        return v_dict
+    def transform(self, texts):
+        """[summary]
 
-    def count_word_based(self, text):
-        word_stats = {}
-        stat = []
-        upper = 0
-        lower = 0
-        parsed = word_tokenize(text)
-        stat = [len(word) for word in parsed]
-        min_len = min(stat)
-        max_len = max(stat)
-        upper = sum([1 if word[0].isupper() else 0 for word in parsed])
-        lower = len(stat) - upper
-        stat = np.array(stat)
-        word_stats['min_len'] = min_len
-        word_stats['max_len'] = max_len
-        word_stats['upper'] = upper
-        word_stats['lower'] = lower
-        word_stats['mean'] = np.mean(stat)
-        word_stats['std'] = np.std(stat)
-        return np.array(list(word_stats.values()))
+        Args:
+            texts ([type]): [description]
+            normalize (bool, optional): [description]. Defaults to True.
 
-    def count_char_based(self, text):
-        char_stats = {'digits': 0, 'letters': 0, 'spaces': 0, 'punct': 0}
-        for c in text:
-            if c.isdigit():
-                char_stats['digits'] = char_stats['digits'] + 1
-            elif c.isalpha():
-                char_stats['letters'] = char_stats['letters'] + 1
-            elif c.isspace():
-                char_stats['spaces'] = char_stats['spaces'] + 1
-            elif c in punctuation:
-                char_stats['punct'] = char_stats['punct'] + 1
-        char_stats.update(self.count_vowels(text))
-        return np.array(list(char_stats.values()))
+        Returns:
+            [type]: [description]
+        """
+        return self.fit(texts)
