fb5d189d0761d6340a8e35ce512d3ad8d1aff466
boshko.koloski <boshko.koloski@gmail.com>
2021-12-11 14:36:54 +0100

    FIX setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 this_directory = Path(__file__).parent
 
-VERSION = '0.0.4'
+VERSION = '0.2'
 DESCRIPTION = 'Representations used in COVID19 detection paper.'
 LONG_DESCRIPTION = long_description = (this_directory /
                                        "README.md").read_text()
@@ -20,7 +20,7 @@ setup(name="c19rep",
       long_description_content_type="text/markdown",
       long_description=LONG_DESCRIPTION,
       packages=find_packages(),
-      install_requires=find_packages(),
+      install_requires=[line.strip() for line in open("requirements.txt").readlines()],
       keywords=['python', 'covid19 fake news detection', 'nlp'],
       classifiers=[
           "Development Status :: 1 - Planning",
